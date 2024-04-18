392539a931faacafcdee60cf8f4121a057201e05
Ignacio Ormaeche Carral <iormaechec01@informatica.iesvalledeljerteplasencia.es>
2024-04-18 14:29:30 +0200

    nav y footer

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,4 @@
 <template>
-
   <div>
     <nav class="navbar navbar-expand-lg navbar-light bg-light mb-5">
       <RouterLink to="/" class="navbar-brand">Carrerita</RouterLink>
@@ -24,7 +23,9 @@
             <RouterLink to="/about" class="nav-link">Go to About</RouterLink>
           </li>
           <li class="nav-item">
-            <Router-link to="/principal" class="nav-link">Go to Principal</Router-link>
+            <Router-link to="/principal" class="nav-link"
+              >Go to Principal</Router-link
+            >
           </li>
           <li class="nav-item dropdown">
             <a
@@ -51,24 +52,44 @@
       </div>
     </nav>
 
-
     <main>
       <RouterView />
     </main>
 
-
     <footer class="footer bg-light mt-1">
       <div class="container">
-        <p class="text-muted">Place sticky footer content here.</p>
+        <ul class="foote_bottom_ul_amrc">
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Services</a></li>
+          <li><a href="#">Pricing</a></li>
+          <li><a href="#">Blog</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+        <p class="text-center">
+          Copyright @2017 | Designed With by <a href="https://iesvallejertepla.educarex.es/" target="_blank" class="text-muted">IES Valle Del Jerte</a>
+        </p>
+
+        <ul class="social_footer_ul">
+          <li>
+            <a href="http://webenlance.com"
+              ><i class="fab fa-facebook-f"></i
+            ></a>
+          </li>
+          <li>
+            <a href="http://webenlance.com"><i class="fab fa-twitter"></i></a>
+          </li>
+          <li>
+            <a href="http://webenlance.com"><i class="fab fa-instagram"></i></a>
+          </li>
+        </ul>
       </div>
     </footer>
   </div>
-
 </template>
 
 
 <style>
-
 .navbar {
   position: absolute;
   width: 100%;
@@ -79,18 +100,59 @@ main {
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  background-color: brown;
+  background-color: lightcoral;
   min-height: 100%;
 }
 
+
 footer {
-  padding: 10px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  position: absolute;
   width: 100%;
-  height: 10vh;
-  
+  background-color: #263238;
+  padding: 10px 0px 25px 0px;
+}
+
+footer p {
+  font-size: 13px;
+  color: #ccc;
+  padding-bottom: 0px;
+  margin-bottom: 8px;
+}
+
+.foote_bottom_ul_amrc {
+  list-style-type: none;
+  padding: 0px;
+  display: table;
+  margin-top: 10px;
+  margin-right: auto;
+  margin-left: auto;
+}
+.foote_bottom_ul_amrc li {
+  display: inline;
+}
+.foote_bottom_ul_amrc li a {
+  color: #999;
+  margin: 0 12px;
+}
+
+.social_footer_ul {
+  display: table;
+  margin: 15px auto 0 auto;
+  list-style-type: none;
+}
+.social_footer_ul li {
+  padding-left: 20px;
+  padding-top: 10px;
+  float: left;
+}
+.social_footer_ul li a {
+  color: #ccc;
+  border: 1px solid #ccc;
+  padding: 8px;
+  border-radius: 50%;
+}
+.social_footer_ul li i {
+  width: 20px;
+  height: 20px;
+  text-align: center;
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,7 @@
 <template>
   <div>
+    <!-- Carrusel -->
+    <!-- Imágenes donde me lleven a diferentes artículos -->
     <div class="container-fluid mt-5">
       <div
         id="carouselExampleControls"
@@ -45,102 +47,95 @@
       <button @click="goToAbout">Go to About</button>
       <button @click="goToPrincipal">Go to Principal</button>
     </div> -->
-    <div class="contenido mb-5">
-      <!-- En este div estará todo el contenido de la página principal sobre la carrera, con información, la ruta y más detalles -->
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque iure
-      eveniet itaque animi soluta molestiae tenetur deleniti magnam expedita.
-      Veritatis, facilis delectus exercitationem maxime, reiciendis officiis
-      nulla repellat unde inventore quae itaque nam. Maxime autem voluptate sunt
-      modi quia, praesentium eos in debitis? Saepe dolores cum accusamus
-      delectus ipsam. Laboriosam libero veniam ratione voluptatibus ea
-      consequatur iusto beatae et debitis, possimus alias sequi deleniti,
-      assumenda id amet harum nobis quasi quis sit facere, corrupti omnis!
-      Cupiditate mollitia possimus impedit dolorem perferendis architecto odit
-      dolores ipsam! Blanditiis rerum architecto harum quaerat praesentium
-      laborum quas in labore dignissimos corporis atque facilis quasi voluptatum
-      impedit quae consectetur, voluptas explicabo inventore porro beatae!
-      Assumenda dignissimos quo nihil animi, rerum sint minus modi officiis? Nam
-      tempore facilis eos pariatur amet inventore at sunt culpa unde delectus
-      nisi ea quae enim impedit odio aliquid praesentium quia, odit eligendi
-      temporibus asperiores doloribus magni similique veniam. Recusandae nam
-      odio eaque nihil magnam minima est in at corporis suscipit quisquam rerum
-      corrupti, aliquam eius minus omnis facere dolorum? Necessitatibus natus
-      cum, saepe modi blanditiis rerum velit sunt magni soluta eos deserunt
-      adipisci possimus dignissimos qui harum, maxime aperiam ad quas quos totam
-      nobis obcaecati doloremque. Fugiat repellat necessitatibus reiciendis
-      nihil recusandae, quaerat, illo expedita provident error, sunt animi rerum
-      officiis quam quo ullam nobis eaque molestias voluptatibus iusto autem.
-      Sit dicta quasi nulla dignissimos aspernatur porro tenetur, nemo alias
-      ullam optio. Iusto obcaecati ullam illum adipisci nam fugit eos incidunt
-      ratione impedit neque, sit veritatis id tempore distinctio unde quis
-      maxime aperiam velit totam, accusamus eius numquam cupiditate eaque quos?
-      Natus enim voluptas dignissimos quaerat repudiandae, dolorum hic doloribus
-      sint a, maxime, voluptates suscipit! Iure, beatae consectetur
-      reprehenderit exercitationem suscipit, aspernatur optio delectus possimus
-      veritatis, dolor dolorum. Iusto, quisquam voluptate rem fugit veritatis ut
-      neque. Nihil delectus similique voluptatum. Magnam vero, nulla esse, totam
-      a laboriosam alias nemo neque saepe, tenetur non odio sint doloremque quos
-      hic voluptatum aliquid nesciunt delectus omnis blanditiis vel soluta nam
-      maxime ut. Sit veritatis error nulla. Culpa atque voluptas incidunt
-      asperiores pariatur. Totam quam, expedita in error reiciendis iusto fugiat
-      ipsum soluta libero, ipsam nisi ipsa, quibusdam quo eius quia ad
-      laboriosam nobis sequi eveniet molestias architecto quidem! Impedit, fuga
-      aliquid, deserunt beatae, sint ratione ut odit aspernatur distinctio optio
-      neque. Vel obcaecati architecto et dignissimos. Obcaecati commodi
-      similique quos quidem dignissimos, quod quas delectus iusto laudantium
-      expedita vel minima aperiam natus modi. Lorem ipsum dolor sit amet
-      consectetur adipisicing elit. Nam, rerum ipsum voluptates assumenda quam
-      atque repudiandae officia molestias, nobis ducimus id quibusdam placeat ut
-      pariatur culpa architecto harum suscipit quisquam. Quae, amet. Cumque
-      nobis aliquid eos aspernatur officiis modi, soluta quis recusandae
-      doloribus, quo esse praesentium consequuntur illo dolorum molestias,
-      perferendis inventore ipsa sit cum molestiae nostrum itaque sapiente!
-      Expedita vero unde facere, illo dolores blanditiis? Maxime, sunt hic nulla
-      aspernatur reprehenderit, inventore nam enim pariatur itaque, aliquam cum
-      ullam a eligendi tempora omnis voluptas voluptatem laudantium ut commodi
-      laborum iste voluptatum vero. Consequuntur quaerat amet modi culpa
-      molestias possimus exercitationem quam asperiores, non, nihil iure libero
-      velit. Tempora dolor illum consequatur dolores nemo sint enim error beatae
-      earum. Similique adipisci quae expedita vero quisquam corporis nobis
-      itaque iste architecto unde? Voluptatem voluptate laboriosam deserunt
-      adipisci fuga eligendi quam. Fuga pariatur enim quasi deleniti perferendis
-      veritatis consequuntur dolor rerum, aut, repellat adipisci itaque expedita
-      qui tenetur quos. Ab, iste, esse illum sapiente fugit culpa ipsa inventore
-      enim explicabo sequi ea cumque a molestias exercitationem? Molestias,
-      porro animi quas unde beatae libero facilis quam eaque et laboriosam
-      fugiat ipsam deserunt nisi! Non aspernatur repellat debitis unde sapiente
-      iusto nulla quidem modi perferendis laboriosam reprehenderit laudantium
-      saepe, voluptatibus quis ex corrupti autem cumque, vitae doloremque? Odit,
-      nemo repellat ut accusamus fuga dolore quam facilis fugiat voluptatibus
-      ullam itaque rem molestiae sequi dolorem tempore, inventore dolor nostrum.
-      Omnis optio aliquid vel nam minus in ab, porro similique soluta voluptatem
-      architecto incidunt impedit ea dolore excepturi enim sequi dicta
-      reiciendis quaerat quis delectus velit fugit mollitia. Illum totam, itaque
-      doloribus vero deserunt nam nemo non officiis consequuntur labore
-      reprehenderit voluptates eligendi quia et fugiat quis exercitationem. Vero
-      quae temporibus fugit aperiam aut explicabo, optio dolor quis ducimus
-      illum odio esse accusamus voluptas odit doloribus sed architecto cum qui
-      omnis beatae ullam ipsam vel reprehenderit. Nesciunt eligendi veniam
-      quaerat deserunt. Voluptatem, totam error. Vero dolorem, veniam pariatur
-      odio nihil est deserunt quae eaque atque, nisi delectus aliquam blanditiis
-      officia dolorum explicabo dicta cum modi natus eius aperiam nam fugit
-      commodi ipsum voluptates? Impedit quos dicta voluptatibus a quidem illo
-      maxime in minus? Reprehenderit, libero optio voluptatibus eveniet rem
-      magni atque non mollitia numquam dolorem. Vero veniam accusamus asperiores
-      consectetur veritatis voluptatem itaque tenetur, aperiam sed culpa omnis
-      nisi, numquam nemo magni repudiandae dolor. At amet, quos accusamus et
-      iste quibusdam obcaecati modi dolorum. Dolor perspiciatis possimus esse
-      ipsa doloremque asperiores porro, ea neque aspernatur nostrum tempore
-      doloribus tenetur qui libero natus mollitia delectus, deserunt id
-      assumenda quaerat! Mollitia sunt voluptatem laudantium recusandae rem eius
-      assumenda nemo iure magnam quibusdam! Illum doloremque rem, pariatur eos
-      nemo explicabo porro quaerat nulla laudantium ipsum, dolore odio inventore
-      nisi! Cumque aliquam architecto facere velit maxime magnam dolores magni
-      iure debitis perferendis. Deleniti nemo distinctio aliquam, totam enim,
-      amet beatae repellendus placeat ducimus ea dolorem. Sunt possimus in
-      voluptate iste iusto consequuntur sit nemo omnis, culpa adipisci
-      assumenda. Minus illum sint maiores iste id! Odit voluptatem eveniet
-      tenetur. Facere, tenetur?
+    <div class="contenido mb-5 px-5">
+      <div class="loermi">
+        <!-- En este div estará todo el contenido de la página principal sobre la carrera, con información, la ruta y más detalles -->
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque
+          iure eveniet itaque animi soluta molestiae tenetur deleniti magnam
+          expedita. Veritatis, facilis delectus exercitationem maxime,
+          reiciendis officiis nulla repellat unde inventore quae itaque nam.
+          Maxime autem voluptate sunt modi quia, praesentium eos in debitis?
+          Saepe dolores cum accusamus delectus ipsam. Laboriosam libero veniam
+          ratione voluptatibus ea consequatur iusto beatae et debitis, possimus
+          alias sequi deleniti, assumenda id amet harum nobis quasi quis sit
+          facere, corrupti omnis! Cupiditate mollitia possimus impedit dolorem
+          perferendis architecto odit dolores ipsam! Blanditiis rerum architecto
+          harum quaerat praesentium laborum quas in labore dignissimos corporis
+          atque facilis quasi voluptatum impedit quae consectetur, voluptas
+          explicabo inventore porro beatae! Assumenda dignissimos quo nihil
+          animi, rerum sint minus modi officiis? Nam tempore facilis eos
+          pariatur amet inventore at sunt culpa unde delectus nisi ea quae enim
+          impedit odio aliquid praesentium quia, odit eligendi temporibus
+          asperiores doloribus magni similique veniam. Recusandae nam odio eaque
+          nihil magnam minima est in at corporis suscipit quisquam rerum
+          corrupti, aliquam eius minus omnis facere dolorum? Necessitatibus
+          natus cum, saepe modi blanditiis rerum velit sunt magni soluta eos
+          deserunt adipisci possimus dignissimos qui harum, maxime aperiam ad
+          quas quos totam nobis obcaecati doloremque. Fugiat repellat
+          necessitatibus reiciendis nihil recusandae, quaerat, illo expedita
+          provident error, sunt animi rerum officiis quam quo ullam nobis eaque
+          molestias voluptatibus iusto autem. Sit dicta quasi nulla dignissimos
+          aspernatur porro tenetur, nemo alias ullam optio. Iusto obcaecati
+          ullam illum adipisci nam fugit eos incidunt ratione impedit neque, sit
+          veritatis id tempore distinctio unde quis maxime aperiam velit totam,
+          accusamus eius numquam cupiditate eaque quos? Natus enim voluptas
+          dignissimos quaerat repudiandae, dolorum hic doloribus sint a, maxime,
+          voluptates suscipit! Iure, beatae consectetur reprehenderit
+          exercitationem suscipit, aspernatur optio delectus possimus veritatis,
+          dolor dolorum. Iusto, quisquam voluptate rem fugit veritatis ut neque.
+          Nihil delectus similique voluptatum. Magnam vero, nulla esse, totam a
+          laboriosam alias nemo neque saepe, tenetur non odio sint doloremque
+          quos hic voluptatum aliquid nesciunt delectus omnis blanditiis vel
+          soluta nam maxime ut. Sit veritatis error nulla. Culpa atque voluptas
+          incidunt asperiores pariatur. Totam quam, expedita in error reiciendis
+          iusto fugiat ipsum soluta libero, ipsam nisi ipsa, quibusdam quo eius
+          quia ad laboriosam nobis sequi eveniet molestias architecto quidem!
+          Impedit, fuga aliquid, deserunt beatae, sint ratione ut odit
+          aspernatur distinctio optio neque. Vel obcaecati architecto et
+          dignissimos. Obcaecati commodi similique quos quidem dignissimos, quod
+          quas delectus iusto laudantium expedita vel minima aperiam natus modi.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, rerum
+          ipsum voluptates assumenda quam atque repudiandae officia molestias,
+          nobis ducimus id quibusdam placeat ut pariatur culpa architecto harum
+          suscipit quisquam. Quae, amet. Cumque nobis aliquid eos aspernatur
+          officiis modi, soluta quis recusandae doloribus, quo esse praesentium
+          consequuntur illo dolorum molestias, perferendis inventore ipsa sit
+          cum molestiae nostrum itaque sapiente! Expedita vero unde facere, illo
+          dolores blanditiis? Maxime, sunt hic nulla aspernatur reprehenderit,
+          inventore nam enim pariatur itaque, aliquam cum ullam a eligendi
+          tempora omnis voluptas voluptatem laudantium ut commodi laborum iste
+          voluptatum vero. Consequuntur quaerat amet modi culpa molestias
+          possimus exercitationem quam asperiores, non, nihil iure libero velit.
+          Tempora dolor illum consequatur dolores nemo sint enim error beatae
+          earum. Similique adipisci quae expedita vero quisquam corporis nobis
+          itaque iste architecto unde? Voluptatem voluptate laboriosam deserunt
+          adipisci fuga eligendi quam. Fuga pariatur enim quasi deleniti
+          perferendis veritatis consequuntur dolor rerum, aut, repellat adipisci
+          itaque expedita qui tenetur quos. Ab, iste, esse illum sapiente fugit
+          culpa ipsa inventore enim explicabo sequi ea cumque a molestias
+          exercitationem? Molestias, porro animi quas unde beatae libero facilis
+          quam eaque et laboriosam fugiat ipsam deserunt nisi! Non aspernatur
+          repellat debitis unde sapiente iusto nulla quidem modi perferendis
+          laboriosam reprehenderit laudantium saepe, voluptatibus quis ex
+          corrupti autem cumque, vitae doloremque? Odit, nemo repellat ut
+          accusamus fuga dolore quam facilis fugiat voluptatibus ullam itaque
+          rem molestiae sequi dolorem tempore, inventore dolor nostrum.
+        </p>
+      </div>
+      <div class="recorrido d-flex justify-content-center">
+        <!-- Descargar el google earth para añadir el recorrido de la carrera. Ver vídeo: https://www.youtube.com/watch?v=c32OEfH5znU -->
+        <iframe
+          class="ruta mt-4"
+          src="https://www.google.com/maps/embed?pb=!1m28!1m12!1m3!1d15863.67207205673!2d-6.094672915530703!3d40.0517309619935!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m13!3e2!4m5!1s0xd3e175708f0d945%3A0x5e9c9d54473a9054!2sVirgen%20del%20Puerto%2C%20Plasencia!3m2!1d40.062011999999996!2d-6.080031399999999!4m5!1s0xd3e1639a468fad7%3A0xf52b2c17bfb4fce4!2sParque%20de%20Los%20Pinos%2C%20Av.%20de%20la%20Hispanidad%2C%20s%2Fn%2C%2010600%20Plasencia%2C%20C%C3%A1ceres!3m2!1d40.0387998!2d-6.0856448!5e1!3m2!1ses!2ses!4v1713424950209!5m2!1ses!2ses"
+          width="70%"
+          height="450"
+          style="border: 0"
+          allowfullscreen=""
+          loading="lazy"
+          referrerpolicy="no-referrer-when-downgrade"
+        ></iframe>
+      </div>
     </div>
   </div>
 </template>
