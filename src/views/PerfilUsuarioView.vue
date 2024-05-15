53698c0b2ec3955d251ad51e99109c9bfc55d197
Ignacio Ormaeche Carral <iormaechec01@informatica.iesvalledeljerteplasencia.es>
2024-05-15 13:49:36 +0200

    diseño perfil

--- src/views/PerfilUsuarioView.vue
+++ src/views/PerfilUsuarioView.vue
@@ -1,46 +1,46 @@
 <!-- HTML de la vista -->
 <template>
 
-  <div class="container principal">
-
-    <div class="row">
-      <div class="col-12">
-        <h1>Mi Perfil</h1>
-      </div>
-    </div>
-
-    <div ref="perfil" class="row w-100">
-      <!-- Muestra la foto de perfil del usuario -->
-      <div class="col-md-4 p-0 text-center">
-        <img :src="imagenUrl" alt="Foto de perfil" class="fotoPerfil m-2">
-      </div>
-      <!-- Muestra los datos del usuario logueado -->
-      <div class="col-md-8 p-0 d-flex justify-content-center">
-        <div class="datosUsuario">
-          <p>Nombre: {{ nombre }}</p>
-          <p>Apellidos: {{ apellidos }}</p>
-          <p>Email: {{ email }}</p>
+  <div>
+    <div class="container principal">
+      <div class="row">
+        <div class="col-12">
+          <h1>Mi Perfil</h1>
         </div>
       </div>
-    </div>
-
-    <div class="row">
-      <div class="col-md-6">
-        <h3>Subir una imagen de perfil</h3>
-        <input type="file" @change="seleccionarFoto" id="fileInput">
-        <v-btn @click="subirFoto" class="btn btn-primary">Subir Archivo</v-btn>
+      <div ref="perfil" class="row w-100">
+        <!-- Muestra la foto de perfil del usuario -->
+        <div class="col-md-12 p-0 text-center">
+          <img :src="imagenUrl" alt="Foto de perfil" class="fotoPerfil m-2">
+        </div>
+        <!-- Muestra los datos del usuario logueado -->
+        <div class="col-md-12 p-0 d-flex justify-content-center">
+          <div class="datosUsuario">
+            <p><span>Nombre:</span> {{ nombre }}</p>
+            <p><span>Apellidos:</span> {{ apellidos }}</p>
+            <p><span>Email:</span> {{ email }}</p>
+          </div>
+        </div>
       </div>
-      <div class="col-md-6 botones">
-        <v-btn @click="desconectarPerfil" class="btn btn-danger">Desconectar</v-btn>
-        <v-btn @click="descargarPDF" class="btn btn-warning">Descargar Dorsal PDF</v-btn>
+      <div class="row w-100 mt-5">
+        <div class="col-md-12 d-flex justify-content-center">
+          <h3 class="mr-2">¿Quiere subir una imagen de perfil? </h3>
+          <input type="file" @change="seleccionarFoto" id="fileInput" class="ml-2">
+        </div>
+        <div class="col-md-12 d-flex justify-content-center mb-5">
+          <v-btn @click="subirFoto" class="btn btn-primary mt-3">Subir Archivo</v-btn>
+        </div>
+        <div class="col-md-12 botones mt-5">
+          <v-btn @click="desconectarPerfil" class="btn btn-danger">Desconectar</v-btn>
+          <v-btn @click="descargarPDF" class="btn btn-warning">Descargar Dorsal PDF</v-btn>
+        </div>
       </div>
-    </div>
-
-    <div class="row">
-      <div class="col-md-12 cambiarPassword">
-        <p>En caso de querer restablecer su contraseña <span id="resetPassword" @click="recuperarContrasenia">Pulse
-            aquí</span>
-        </p>
+      <div class="row w-100">
+        <div class="col-md-12 cambiarPassword d-flex justify-content-center">
+          <p>En caso de querer restablecer su contraseña <span id="resetPassword" @click="recuperarContrasenia">Pulse
+              aquí</span>
+          </p>
+        </div>
       </div>
     </div>
   </div>
@@ -243,6 +243,8 @@ export default {
   min-height: 80vh;
   background-color: lightgray;
   box-shadow: 15px 10px 10px rgba(0, 0, 0, 0.1);
+  border-radius: 4%;
+  margin: 30px 0 30px 0;
 }
 
 
@@ -268,14 +270,21 @@ export default {
   font-size: 1.2em;
 }
 
+.datosUsuario span {
+  font-weight: bold;
+}
 
 .botones {
   display: flex;
-  justify-content: flex-end;
   align-items: center;
+  justify-content: center;
   width: 100%;
   cursor: pointer;
   height: 7vh;
+}
+
+.botones .btn {
+  margin: 0 10px;
 }
 
 .btn {
