9aa9143bbb02234527ca341c956bc70a52c49fbc
Ignacio Ormaeche Carral <iormaechec01@informatica.iesvalledeljerteplasencia.es>
2024-05-21 16:27:41 +0200

    bootstrapvue

--- src/main.js
+++ src/main.js
@@ -3,8 +3,11 @@ import App from './App.vue'
 import router from './router'
 import { createVuetify } from 'vuetify'
 import 'vuetify/styles'
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import { BootstrapVue } from 'bootstrap-vue';
 
 const app = createApp(App);
 const vuetify = createVuetify();
 
-app.use(router).use(vuetify).mount('#app')
+app.use(router).use(vuetify).app.use(BootstrapVue).mount('#app')
